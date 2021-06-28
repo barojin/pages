2fbed1c116d55241730616f62ddc4fb20458e5f9
Hojin Nam <hojinnam@HojindeMacBook-Pro.local>
2021-06-28 16:41:59 -0700

    ctd

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -22,8 +22,8 @@ render() {
             <div className="d-flex flex-column flex-md-row justify-content-between">
                 <div className="flex-grow-1">
                     <h3 className="mb-0">
-                        <a className="social-icon"  href="https://github.com/barojin/DjangoTutorial"><i className="fab fa-github" /></a>
-                        Polls Web App <span className="text-primary">[python, Django framework, Sqlite3]</span>
+                        <a className="social-icon"  href="https://github.com/barojin/DjangoTutorial"><i className="fab fa-github" />
+                        </a> Polls Web App <span className="text-primary">[python, Django framework, Sqlite3]</span>
                     </h3>
                     <p> Create the web site that shows polls and vote in them and admin site provide the add, change, and delete polls.
                         It contains how to handle the route, models in the Database, automated testing, views and templates, admin page conventions,
