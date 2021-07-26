73c1076d87efeff0b9ac453b34f7beb6ae39eddd
Hojin Nam <hojinnam@HojindeMacBook-Pro.local>
2021-07-26 11:57:01 -0700

    changed the main page message

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -16,8 +16,8 @@ render() {
         <a>San Mateo, California, United States<br></br></a>
         <a href="mailto:name@email.com"> barojins@gmail.com</a>
       </div>
-      <p className="lead mb-5">Software developer based on CS fundamentals<br></br>
-      writing program codes, using software frameworks and tools.</p>
+      <p className="lead mb-5">Software engineer based on CS fundamentals<br></br>
+      deliver a solid software product with frameworks and tools.</p>
       <div className="social-icons">
         <a className="social-icon" href="https://www.linkedin.com/in/barojin/"><i className="fab fa-linkedin-in" /></a>
         <a className="social-icon" href="https://github.com/barojin/"><i className="fab fa-github" /></a>        
